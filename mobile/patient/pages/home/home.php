3787c866df28ec026580a26b006fb613be65a003
negiyogi <negi.yogender98@gmail.com>
2025-09-15 11:28:10 +0530

    Commit only home.php changes

--- mobile/patient/pages/home/home.php
+++ mobile/patient/pages/home/home.php
@@ -127,9 +127,6 @@
               $payment_booking = "";
           }
 
-
-
-          
           $switch_selection = get_user_data_new($data["data_global"]["mobile"],$data["data_global"]["id"]);
 
           $data_record = array();
