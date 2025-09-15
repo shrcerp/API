022e972cf7c3023000f73e742a9e4c37784502d6
negiyogi <negi.yogender98@gmail.com>
2025-09-15 17:06:47 +0530

    coupon query upodate

--- mobile/patient/pages/coupon/coupon.php
+++ mobile/patient/pages/coupon/coupon.php
@@ -3,13 +3,13 @@
     function get_coupon_data($data){
         global $con;
         $date = date("Y-m-d");
-        // print_r($data);
+        // print_r(json_encode($data));
         // return;
         $coupon_code = $_POST["coupon_code"];
         $location = $data["data_self"]["location"];
        
         // $location = 'sarvodaya-hospital-research-centre-sector-8';
-        $sql = "select * from video_patient_coupon where coupon_code = '$coupon_code' and status = 1 and start_date <= '$date' and end_date >= '$date' and location='$location'";
+        $sql = "select * from video_patient_coupon where coupon_code = '$coupon_code' and status = 1 and start_date <= '$date' and end_date >= '$date' and FIND_IN_SET('$location', location)";
         $result_data = mysqli_query($con, $sql);
         $row = mysqli_fetch_assoc($result_data);
         if(!is_array($row)){
