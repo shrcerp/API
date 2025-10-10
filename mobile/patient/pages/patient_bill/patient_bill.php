962003dd415b49171fa1f953041c03d6b9512cb3
negiyogi <negi.yogender98@gmail.com>
2025-10-10 15:40:49 +0530

    patient bill

---
+++ mobile/patient/pages/patient_bill/patient_bill.php
@@ -0,0 +1,52 @@
+<?php
+function patient_bill($data){
+    global $con;
+    
+    // $mrn = $data['data_global']['mrn'];
+    $mrn = 'SR588436';
+    // $ip = $data['data_self']['ip'];
+    $ip = 'IP123';
+
+    $sql = "SELECT * FROM patient_acknowledgement WHERE mrn = '$mrn' AND ip = '$ip' and status=1";
+    $query = mysqli_query($con, $sql);
+
+    if ($query && mysqli_num_rows($query) > 0) {
+        $result = mysqli_fetch_assoc($query);
+        return [
+                        "title"=> "",
+                        "layout_code"=> "73-1",
+                        "layout_des"=> "info_card",
+                        "sub_text"=> "",
+                        "image"=> "https://sarvodayahospital19.com//api/mobile/images/sarvodaya_mobile_logo.png",
+                        "timestamp"=> "",
+                        "web_link"=> "",
+                        "web_view"=> "0",
+                        "click_action"=> "0",
+                        "web_view_heading"=> "",
+                        "page_code"=> "5020",
+                        "next_page"=> [],
+                        "elements"=> $result
+        ];
+    }else {
+        return array(
+            "code" => "101",
+            "message" => "No Data Found for this MRN",
+            "result" => array(
+                "title" => "No Data Found for this MRN",
+                "layout_code" => "1",
+                "layout_des" => "info_card",
+                "sub_text" => "",
+                "image" => "",
+                "timestamp" => "",
+                "web_link" => "",
+                "web_view" => "0",
+                "click_action" => "0",
+                "web_view_heading" => "",
+                "page_code" => "5020",
+                "next_page" => [],
+                "elements" => []  
+            )
+        );
+    }
+}
+?>
