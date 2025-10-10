8ed64b4bd10ff10f48bfd5972bc5d4d9fd2e1d8f
negiyogi <negi.yogender98@gmail.com>
2025-10-10 13:38:30 +0530

    doctor lab added data self location

--- mobile/patient/pages/doctor_lab/doctor_lab.php
+++ mobile/patient/pages/doctor_lab/doctor_lab.php
@@ -3,6 +3,28 @@
 
         $mrn = $data['data_global']['mrn'];
         // $mrn = 'SR674812';
+        $location = $data['data_self']['location'];
+        if(empty($location)){
+            return array(
+                "code"=>"101",
+                "message"=>"please select location",
+                "result" => [
+                     "title"=> "location not found",
+                    "layout_code"=> "1",
+                    "layout_des"=> "info_card",
+                    "sub_text"=> "",
+                    "image"=> "",
+                    "timestamp"=> "",
+                    "web_link"=> "",
+                    "web_view"=> "0",
+                    "click_action"=> "0",
+                    "web_view_heading"=> "",
+                    "page_code"=> "5020",
+                    "next_page"=> [],
+                    "elements"=> []
+                ]
+            );
+        }
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
@@ -15,7 +37,7 @@
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => array(
-            'mrn' => $mrn
+            'mrn' => $mrn, 'location'=> $location
         ),
         CURLOPT_HTTPHEADER => array(
             'Cookie: ci_session=eki08hh4pqcdvt40k1lq97fp7t41sdbr'
